fd6a53fee1839c45ebce01550e01dc5346efc420
ushela <66378950+ushela@users.noreply.github.com>
2020-10-24 13:30:51 +0100

    adding robofriend

---
+++ src/containers/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+// import { robots } from './robots';
+import Scroll from '../components/Scroll.js';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+
+
+
+
+class App extends Component  {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }    
+    }
+
+componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(user => this.setState({ robots: user}));
+   
+}
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value })
+         
+    }
+
+    render() {
+        const { robots, searchfield } =this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        }) 
+
+        return !robots.length ?
+          <h1>Loading</h1> : 
+            (
+        <div className='tc'>
+            <h1 className='f2'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                <ErrorBoundry>
+                    <CardList robots={filteredRobots} />
+                </ErrorBoundry>
+                </Scroll>
+        </div>
+            );
+    }
+}
+
+export default App;
